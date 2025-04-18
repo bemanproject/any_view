86e84a7521bdcaa85bd7290fe4e41313bc22ed08
Radu Nichita <radunichita99@gmail.com>
2025-04-18 03:14:52 +0300

    Add more easy to follow example

--- examples/sum_views.cpp
+++ examples/sum_views.cpp
@@ -9,26 +9,22 @@
 namespace bav = beman::any_view;
 using opt = bav::any_view_options;
 
-// Type alias for a generic input-range-like view over elements of type T.
-// This wraps `bav::any_view` to abstract over different underlying range types
-template <class T>
-using proxy_any_view = bav::any_view<T, opt::input, T>;
-
-// Computes the sum of all integers in a view of views of integers.
-constexpr auto sum(proxy_any_view<proxy_any_view<int>> views) {
+auto sum(bav::any_view<int, opt::input> v1, bav::any_view<int, opt::input> v2) {
     auto result = 0;
 
-    for (auto view : views) {
-        for (const auto value : view) {
-            result += value;
-        }
+    for (auto elem : v1) {
+        result = result + elem;
+    }
+
+    for (auto elem : v2) {
+        result = result + elem;
     }
 
     return result;
 }
 
 int main() {
-    auto res = sum(std::vector{std::vector{1, 2}, std::vector{3, 4}});
+    auto res = sum(std::vector{1, 2}, std::array<int, 2>{3, 4});
     std::cout << res << "\n";
     return 0;
 }
