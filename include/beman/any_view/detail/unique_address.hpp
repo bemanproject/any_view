849eaa87a8d206fdb20efc669a9161c0f6caf890
Jeff Garland <jeff@crystalclearsoftware.com>
2025-07-05 08:49:11 -0700

    refactor unique_address logic into a single location

---
+++ include/beman/any_view/detail/unique_address.hpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef BEMAN_ANY_VIEW_DETAIL_UNIQUE_ADDRESS_HPP
+#define BEMAN_ANY_VIEW_DETAIL_UNIQUE_ADDRESS_HPP
+
+#include <beman/any_view/config.hpp>
+
+#endif
