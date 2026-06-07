dfba413895f2fb829f8e02bb44dbd7c1bdad8acd
Patrick Roberts <git@patrickroberts.dev>
2026-06-06 21:50:14 -0500

    Add lifetimebound attribute

--- include/beman/any_view/detail/no_unique_address.hpp
+++ include/beman/any_view/detail/no_unique_address.hpp
@@ -4,11 +4,15 @@
 #define BEMAN_ANY_VIEW_DETAIL_NO_UNIQUE_ADDRESS_HPP
 
 #ifndef BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS
-    #if _MSC_VER
+    #ifndef __has_cpp_attribute
+        #define BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS
+    #elif __has_cpp_attribute(msvc::no_unique_address)
         #define BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
-    #else
+    #elif __has_cpp_attribute(no_unique_address)
         #define BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS [[no_unique_address]]
-    #endif // _MSC_VER
+    #else
+        #define BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS
+    #endif // __has_cpp_attribute
 #endif     // BEMAN_ANY_VIEW_NO_UNIQUE_ADDRESS
 
 #endif // BEMAN_ANY_VIEW_DETAIL_NO_UNIQUE_ADDRESS_HPP
